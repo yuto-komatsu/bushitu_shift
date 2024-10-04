aff204212c6f0716d94aebfbd3f45d5a89456dc3
yuto-komatsu <116863727+yuto-komatsu@users.noreply.github.com>
2024-10-04 11:57:34 +0900

    Update 部室練習シフト作成プログラム.py

--- 部室練習シフト作成プログラム.py
+++ 部室練習シフト作成プログラム.py
@@ -300,7 +300,7 @@ with tab3:
                     value = 0
 
                 # セッション状態に保存
-                k = {(i, d, t):value}
+                k[i, d, t] = int(sheet_band.cell(row=2 + t, column=2 + d).value == 1)
       # st.session_state["kibou_time"] = k      
       # st.write(st.session_state["kibou_time"])
       st.write(k)
