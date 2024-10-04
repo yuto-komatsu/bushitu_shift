1d4e61b5e8fa749eb1edd88243c0f771bebb127b
yuto-komatsu <116863727+yuto-komatsu@users.noreply.github.com>
2024-10-04 10:55:02 +0900

    Update 部室練習シフト作成プログラム.py

--- 部室練習シフト作成プログラム.py
+++ 部室練習シフト作成プログラム.py
@@ -309,15 +309,15 @@ with tab4:
   if st.session_state["kibou_file"] is not None:
     st.session_state["book1"] = load_workbook(st.session_state["kibou_file"])
     
-    for i in band_list:
+    for i in range(1, band_sum+1):
       st.write(i)
-    #   sheet_band = st.session_state["book1"][band_list[i]]
-    #   for d in D:
-    #     values = [sheet_band.cell(row=2 + t, column=2 + d).value for t in T]
-    #     kibou[i, d] = int(any(v is not None and v > 0 for v in values))
-    #     for t in T:
-    #       st.session_state["kibou_time"][i, d, t] = int(sheet_band.cell(row=2 + t, column=2 + d).value == 1)
-    # st.write(st.session_state["kibou_time"])
+      sheet_band = st.session_state["book1"][band_list[i]]
+      for d in D:
+        values = [sheet_band.cell(row=2 + t, column=2 + d).value for t in T]
+        kibou[i, d] = int(any(v is not None and v > 0 for v in values))
+        for t in T:
+          st.session_state["kibou_time"][i, d, t] = int(sheet_band.cell(row=2 + t, column=2 + d).value == 1)
+    st.write(st.session_state["kibou_time"])
   else:
     st.write("１で参加バンドを読み込ませてください。")
 
