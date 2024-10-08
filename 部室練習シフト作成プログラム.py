bebc223595e26cae99beaeb7aa711801f5d9a22c
yuto-komatsu <116863727+yuto-komatsu@users.noreply.github.com>
2024-10-08 13:41:47 +0900

    Update 部室練習シフト作成プログラム.py

--- 部室練習シフト作成プログラム.py
+++ 部室練習シフト作成プログラム.py
@@ -120,7 +120,7 @@ def kinshi_select():
     st.success('リセットされました。')
 
   # 追加された日付と今日の日付の差分を辞書に登録（キーを1, 2, 3, ...とする）
-  st.session_state["st.session_state["kinshi"]"] = {int(i + 1): (d - st.session_state["start_day"] + datetime.timedelta(days=1)).days for i, d in enumerate(st.session_state['dates_list'])}
+  st.session_state["kinshi"] = {int(i + 1): (d - st.session_state["start_day"] + datetime.timedelta(days=1)).days for i, d in enumerate(st.session_state['dates_list'])}
 
   # 現在の追加済みの日付を表示
   st.write('追加された日付一覧:')
