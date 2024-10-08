521794cd8b711eaf58053d67ec93c620975312d2
yuto-komatsu <116863727+yuto-komatsu@users.noreply.github.com>
2024-10-08 13:49:08 +0900

    Update 部室練習シフト作成プログラム.py

--- 部室練習シフト作成プログラム.py
+++ 部室練習シフト作成プログラム.py
@@ -234,13 +234,15 @@ def saitekika():
               if st.session_state["kibou_time"][f"{i}_{d}_{t}"] == 0:
                   model += y[i, d, t] == 0
 
+  
   # 部室利用禁止日に練習を割り当てない
-  for d in st.session_state["D"]:
-      for k in st.session_state["kinshi"]:
-          if d == st.session_state["kinshi"][k]:
-            for i in st.session_state["I"]:
-              for t in T:
-                model += y[i, d, t] == 0
+  if st.session_state["kinshi"] is not None:
+    for d in st.session_state["D"]:
+        for k in st.session_state["kinshi"]:
+            if d == st.session_state["kinshi"][k]:
+              for i in st.session_state["I"]:
+                for t in T:
+                  model += y[i, d, t] == 0
 
 
   # 最終週に希望がある場合、必ず練習を入れる
