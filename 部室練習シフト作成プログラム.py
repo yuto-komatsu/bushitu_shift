fff252729dd64ff359213c023b5109e880943e6f
yuto-komatsu <116863727+yuto-komatsu@users.noreply.github.com>
2024-10-14 23:37:09 +0900

    Update 部室練習シフト作成プログラム.py

--- 部室練習シフト作成プログラム.py
+++ 部室練習シフト作成プログラム.py
@@ -79,6 +79,38 @@ week = {}
 st.session_state["page_control"] = 0
 st.session_state["kinshi"] = {}
 
+
+def wakusen(sheet):
+  #枠線作成
+  #B列目
+  sheet.cell(row=2, column=2).border = border_topleft
+  for t in range(1,8):
+    sheet.cell(row=2+t, column=2).border = border_left
+  sheet.cell(row=9, column=2).border = border_bottomleft
+  #2行目
+  for j in range(1, day_sum):
+    sheet.cell(row=2, column=2+j).border = border_topcenter
+  #右端
+  sheet.cell(row=2, column=day_sum+2).border = border_topright
+  for t in range(1,8):
+    sheet.cell(row=2+t, column=day_sum+2).border = border_right
+  sheet.cell(row=9, column=day_sum+2).border = border_bottomright
+  #中
+  for i in range(1, day_sum):
+    for t in range(1,7):
+      sheet.cell(row=2+t, column=2+i).border = border_allthin
+  #下
+  for j in range(1, day_sum):
+    sheet.cell(row=9, column=2+j).border = border_bottomcenter
+
+  #書式設定
+  font = Font(name="游ゴシック",size=11,bold=True)
+  for t in range(1, 9):
+    for j in range(1, day_sum+2):
+      sheet.cell(row=1+t, column=1+j).font = font
+      sheet.cell(row=1+t, column=1+j).alignment = Alignment(horizontal = 'left', vertical = 'center')
+
+
 def change_page():
   # ページ切り替えボタンコールバック
   st.session_state["page_control"] += 1
@@ -164,34 +196,36 @@ def input_date():
         j += 1
         calc_day += datetime.timedelta(days=1)
 
-    #枠線作成
-    #B列目
-    sheet.cell(row=2, column=2).border = border_topleft
-    for t in range(1,8):
-      sheet.cell(row=2+t, column=2).border = border_left
-    sheet.cell(row=9, column=2).border = border_bottomleft
-    #2行目
-    for j in range(1, day_sum):
-      sheet.cell(row=2, column=2+j).border = border_topcenter
-    #右端
-    sheet.cell(row=2, column=day_sum+2).border = border_topright
-    for t in range(1,8):
-      sheet.cell(row=2+t, column=day_sum+2).border = border_right
-    sheet.cell(row=9, column=day_sum+2).border = border_bottomright
-    #中
-    for i in range(1, day_sum):
-      for t in range(1,7):
-        sheet.cell(row=2+t, column=2+i).border = border_allthin
-    #下
-    for j in range(1, day_sum):
-      sheet.cell(row=9, column=2+j).border = border_bottomcenter
+    wakusen(sheet)
 
-    #書式設定
-    font = Font(name="游ゴシック",size=11,bold=True)
-    for t in range(1, 9):
-      for j in range(1, day_sum+2):
-        sheet.cell(row=1+t, column=1+j).font = font
-        sheet.cell(row=1+t, column=1+j).alignment = Alignment(horizontal = 'left', vertical = 'center')
+    # #枠線作成
+    # #B列目
+    # sheet.cell(row=2, column=2).border = border_topleft
+    # for t in range(1,8):
+    #   sheet.cell(row=2+t, column=2).border = border_left
+    # sheet.cell(row=9, column=2).border = border_bottomleft
+    # #2行目
+    # for j in range(1, day_sum):
+    #   sheet.cell(row=2, column=2+j).border = border_topcenter
+    # #右端
+    # sheet.cell(row=2, column=day_sum+2).border = border_topright
+    # for t in range(1,8):
+    #   sheet.cell(row=2+t, column=day_sum+2).border = border_right
+    # sheet.cell(row=9, column=day_sum+2).border = border_bottomright
+    # #中
+    # for i in range(1, day_sum):
+    #   for t in range(1,7):
+    #     sheet.cell(row=2+t, column=2+i).border = border_allthin
+    # #下
+    # for j in range(1, day_sum):
+    #   sheet.cell(row=9, column=2+j).border = border_bottomcenter
+
+    # #書式設定
+    # font = Font(name="游ゴシック",size=11,bold=True)
+    # for t in range(1, 9):
+    #   for j in range(1, day_sum+2):
+    #     sheet.cell(row=1+t, column=1+j).font = font
+    #     sheet.cell(row=1+t, column=1+j).alignment = Alignment(horizontal = 'left', vertical = 'center')
 
 
     # デフォルトで作成されるシートを削除
@@ -301,35 +335,35 @@ def result():
             if st.session_state["y2"][f"{i}_{d}_{t}"] > 0.01:
                 sheet.cell(row=2 + t, column=2 + d).value = band_list[i]
 
+  wakusen(sheet)
+# #枠線作成
+#   #B列目
+#   sheet.cell(row=2, column=2).border = border_topleft
+#   for t in range(1,8):
+#     sheet.cell(row=2+t, column=2).border = border_left
+#   sheet.cell(row=9, column=2).border = border_bottomleft
+#   #2行目
+#   for j in range(1, day_sum):
+#     sheet.cell(row=2, column=2+j).border = border_topcenter
+#   #右端
+#   sheet.cell(row=2, column=day_sum+2).border = border_topright
+#   for t in range(1,8):
+#     sheet.cell(row=2+t, column=day_sum+2).border = border_right
+#   sheet.cell(row=9, column=day_sum+2).border = border_bottomright
+#   #中
+#   for i in range(1, day_sum):
+#     for t in range(1,7):
+#       sheet.cell(row=2+t, column=2+i).border = border_allthin
+#   #下
+#   for j in range(1, day_sum):
+#     sheet.cell(row=9, column=2+j).border = border_bottomcenter
 
-#枠線作成
-  #B列目
-  sheet.cell(row=2, column=2).border = border_topleft
-  for t in range(1,8):
-    sheet.cell(row=2+t, column=2).border = border_left
-  sheet.cell(row=9, column=2).border = border_bottomleft
-  #2行目
-  for j in range(1, day_sum):
-    sheet.cell(row=2, column=2+j).border = border_topcenter
-  #右端
-  sheet.cell(row=2, column=day_sum+2).border = border_topright
-  for t in range(1,8):
-    sheet.cell(row=2+t, column=day_sum+2).border = border_right
-  sheet.cell(row=9, column=day_sum+2).border = border_bottomright
-  #中
-  for i in range(1, day_sum):
-    for t in range(1,7):
-      sheet.cell(row=2+t, column=2+i).border = border_allthin
-  #下
-  for j in range(1, day_sum):
-    sheet.cell(row=9, column=2+j).border = border_bottomcenter
-
-  #書式設定
-  font = Font(name="游ゴシック",size=11,bold=True)
-  for t in range(1, 9):
-    for j in range(1, day_sum+2):
-      sheet.cell(row=1+t, column=1+j).font = font
-      sheet.cell(row=1+t, column=1+j).alignment = Alignment(horizontal = 'left', vertical = 'center')
+#   #書式設定
+#   font = Font(name="游ゴシック",size=11,bold=True)
+#   for t in range(1, 9):
+#     for j in range(1, day_sum+2):
+#       sheet.cell(row=1+t, column=1+j).font = font
+#       sheet.cell(row=1+t, column=1+j).alignment = Alignment(horizontal = 'left', vertical = 'center')
 
   
   buffer2 = BytesIO()
@@ -393,7 +427,7 @@ if "page_control" in st.session_state and st.session_state["page_control"] == 1:
     if d == True:
       kinshi_select()
     week_judge(st.session_state["start_day"], vacation)
-
+    
     if st.button("入力完了"):
       st.session_state["3pagenext"] = True
     if st.session_state["3pagenext"]:
