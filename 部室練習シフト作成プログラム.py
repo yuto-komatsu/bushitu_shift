fb66bbd4ad29c61877aed720b625249c82b96d5d
yuto-komatsu <116863727+yuto-komatsu@users.noreply.github.com>
2024-11-29 16:49:56 +0900

    Update 部室練習シフト作成プログラム.py

--- 部室練習シフト作成プログラム.py
+++ 部室練習シフト作成プログラム.py
@@ -821,6 +821,7 @@ def part_shift_main():
         for t in st.session_state["T"]:
           value = sheet.cell(row=10+i, column=4+t).value
           c[i, t] = value if value is not None else 1
+          st.write(c[i, t])
     
       g = {} #1回生の講習会参加
       for i in range(st.session_state["n2"]+st.session_state["n3"]+1,st.session_state["n1"]+st.session_state["n2"]+st.session_state["n3"]+1):
