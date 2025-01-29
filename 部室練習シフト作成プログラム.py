5f452213e7a237c8f56900989ffc2f728c1afa0d
yuto-komatsu <116863727+yuto-komatsu@users.noreply.github.com>
2025-01-29 13:15:03 +0900

    Update 部室練習シフト作成プログラム.py

--- 部室練習シフト作成プログラム.py
+++ 部室練習シフト作成プログラム.py
@@ -340,6 +340,8 @@ def result():
           fill = PatternFill(patternType='solid', fgColor='d3d3d3')
           for t in range(1, 9):
             sheet.cell(row=t+1, column=i+2).fill = fill
+            sheet.merge_cells(start_row=3, start_column=i, end_row=9, end_column=i)
+
           
     
       j += 1
